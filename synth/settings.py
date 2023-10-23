5b595733c80fb5bd21fa9307c8a847067b48856e
Jonas Thorell <jthorell@gmail.com>
2023-10-24 01:18:23 +0200

    Change to a different bucket for debugging purposes

--- synth/settings.py
+++ synth/settings.py
@@ -170,7 +170,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'synth-vinyls-e3c70f9ba271.herokuapp.com'
+    AWS_STORAGE_BUCKET_NAME = 'synth-jt'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
