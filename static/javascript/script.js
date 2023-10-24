7e839519ea1fcab9f4d8adb3f57b97c1c984ec12
Jonas Thorell <jthorell@gmail.com>
2023-10-24 02:01:01 +0200

    Add: initial folder structure for css/js and some classes to start with

---
+++ static/javascript/script.js
@@ -0,0 +1,21 @@
+
+const triggers = [
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+    'light',
+    'dark',
+];
+const basicInstances = [
+    'alert-primary',
+    'alert-secondary',
+    'alert-success',
+    'alert-danger',
+    'alert-warning',
+    'alert-info',
+    'alert-light',
+    'alert-dark',
+];
