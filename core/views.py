c9a9ba6d4e7b434fd753f388d13c1ef2720170ec
Jonas Thorell <jthorell@gmail.com>
2023-10-31 13:51:45 +0100

    Add: enabled and tested the messaging framework

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -8,3 +9,10 @@ class Index(TemplateView):
     '''Class used to display the index page '''
 
     template_name = 'core/index.html'
+    
+    # def get(self, request, *args, **kwargs):
+    #     super().get(request, *args, **kwargs)
+        
+    #     art_mess = "You do not have permission to delete that comment."
+    #     messages.info(request, art_mess)
+    #     return HttpResponseRedirect("/webauth")
