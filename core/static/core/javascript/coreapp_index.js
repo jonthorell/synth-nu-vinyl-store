8438bdf469f949f33b8ba21f16a90e15a02ac889
Jonas Thorell <jthorell@gmail.com>
2023-11-02 23:30:38 +0100

    Add: some additional styling on landing page to make it look better and to adapt to lower resolutions better

--- core/static/core/javascript/coreapp_index.js
+++ core/static/core/javascript/coreapp_index.js
@@ -1,15 +1,19 @@
 
 $(window).resize(function () {
-    /*Remove or add the border classes depending on screensize. The classes provides the vertical line between the divs*/
+    /*Remove or add the border classes depending on screensize. The classes provides the vertical line between the divs
+    Also hide/show all spans with the published date
+    */
     if($(window).width() < 768)
     {
         $("#left_div").removeClass('border-end')
         $("#right_div").removeClass('border-start')
+        $(".published").hide()
     }
 
     if ($(window).width() > 768) {
         $("#left_div").addClass('border-end')
         $("#right_div").addClass('border-start')
+        $(".published").show()
     }
 
 });
