b74a9fa317903244677402210177719db01f9e08
Jonas Thorell <jthorell@gmail.com>
2023-11-04 20:07:16 +0100

    Add: customized admin for product app

--- products/admin.py
+++ products/admin.py
@@ -4,7 +4,43 @@ from .models import product, genre, mediatype, artist
 
 # Register models in admin
 
-admin.site.register(product)
-admin.site.register(genre)
-admin.site.register(mediatype)
-admin.site.register(artist)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'genre',
+        'price',
+        'rating',
+        'image',
+        'slug',
+     )
+    
+    ordering = ("sku",)
+    
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'slug',
+        )
+    
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'slug',
+        )
+    
+class MediaTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'slug',
+        )
+        
+
+
+admin.site.register(product,ProductAdmin)
+admin.site.register(genre, GenreAdmin)
+admin.site.register(mediatype, MediaTypeAdmin)
+admin.site.register(artist,ArtistAdmin)
