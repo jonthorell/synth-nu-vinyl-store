b7a825b924e052a81b9e74c511851035d2a84614
Jonas Thorell <jthorell@gmail.com>
2023-11-04 15:50:45 +0100

    Add: new arrivals div displays data from db instead of placeholders

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 
 from .models import newsfeed
+from products.models import product
 
 # Create your views here.
 
@@ -15,6 +16,7 @@ class Index(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['newsfeeds'] = newsfeed.objects.filter(status=1).order_by('-created_on')[:10]
+        context['products'] = product.objects.all().order_by('-created_on')[:10]
         # return the last ten articles to template
 
         return context
--- products/models.py
+++ products/models.py
@@ -57,6 +57,8 @@ class product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
