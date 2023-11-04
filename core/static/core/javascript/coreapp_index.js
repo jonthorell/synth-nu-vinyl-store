e3c4ed478536727cf513b256fec577fc7713de93
Jonas Thorell <jthorell@gmail.com>
2023-11-04 21:14:48 +0100

    Add: changed the dateformat on landing page

--- core/static/core/javascript/coreapp_index.js
+++ core/static/core/javascript/coreapp_index.js
@@ -1,18 +1,24 @@
 
 $(window).resize(function () {
-    /*Remove or add the border classes depending on screensize. The classes provides the vertical line between the divs
-    Hide/show the spans with the published date
-    */
+    /*Remove or add the border classes depending on screensize. The classes provides the vertical line between the divs */
     if($(window).width() < 768)
     {
         $("#left_div").removeClass('border-end')
         $("#right_div").removeClass('border-start')
-        $(".published").hide()
     }
 
     if ($(window).width() > 768) {
         $("#left_div").addClass('border-end')
         $("#right_div").addClass('border-start')
+    }
+
+    /* Hide/show the spans with the published date */
+
+    if ($(window).width() < 500) {
+        $(".published").hide()
+    }
+
+    if ($(window).width() > 500) {
         $(".published").show()
     }
 
