cfc39bd20b6379e32403eef8b65f013a829906f0
Jonas Thorell <jthorell@gmail.com>
2023-11-08 23:06:47 +0100

    Add: set up allauth

--- synth/settings.py
+++ synth/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     # my custom apps
     'core',
     'bag',
@@ -61,9 +62,16 @@ INSTALLED_APPS = [
     'crispy_forms',
     # for aws
     'storages',
+    # allauth
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
 ]
 
 SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -71,7 +79,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/success'
 
 MESSAGE_TAGS = {
         messages.DEBUG: 'alert-info',
@@ -112,13 +120,13 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            # os.path.join(BASE_DIR, 'templates','allauth')
+            os.path.join(BASE_DIR, 'templates','allauth')
             ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request', #required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -129,6 +137,14 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 
 WSGI_APPLICATION = 'synth.wsgi.application'
 
--- synth/urls.py
+++ synth/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('about/', include("about.urls"), name="about-urls"),
     path('profiles/', include("profiles.urls"), name="profiles-urls"),
     path('products/', include("products.urls"), name="products-urls"),
+    path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'synth.views.handler404'
