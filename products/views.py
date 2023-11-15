cf7a5384fc39aebe0ef282a8bdd528b00779d14e
Jonas Thorell <jthorell@gmail.com>
2023-11-15 01:36:02 +0100

    Add: displaying number search results in all_products

--- products/views.py
+++ products/views.py
@@ -25,7 +25,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'genre':
+                sortkey = 'genre__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
