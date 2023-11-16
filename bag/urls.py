21aa5cda299ff5e243449456278d9d8afbbad572
Jonas Thorell <jthorell@gmail.com>
2023-11-17 00:13:09 +0100

    Add: add-to-bag form is present on product_details page. Looks good and is responsive. Only shown if the product in question is in stock. Only accepts numbers between 1 and the number of items in stock.

--- bag/urls.py
+++ bag/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = "view_bag"
-
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
     ]
