7f0b6627ed4ac58bbe33594a93958c2cc0dd6bf6
Jonas Thorell <jthorell@gmail.com>
2023-11-20 01:42:31 +0100

    Change: the toast shown when adding a product now displays the product name

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,8 @@
+
 from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
+
+from products.models import product
 
 # Create your views here.
 
@@ -11,6 +14,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    prod = product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -20,7 +24,7 @@ def add_to_bag(request, item_id):
     else:
         bag[item_id] = quantity
 
-    messages.info(request, "Product added to your shopping bag.") 
+    messages.info(request, f'Added "{prod.name}" to your bag.') 
     request.session['bag'] = bag
         
     return redirect(redirect_url)
