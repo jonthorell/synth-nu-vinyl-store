fc9439eff3cf0fe791412abf2ad94b6f342d29bc
Jonas Thorell <jthorell@gmail.com>
2023-11-21 01:45:55 +0100

    Add: order model for checkout is created and available in django admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from .models import Order, OrderLineItem
 
-# Register your models here.
+# Register models in admin
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'order_number',
+        'full_name',
+        'email',
+        'phone_number',
+        'country',
+        'postcode',
+        'street_address1',
+        'street_address2',
+        'county',
+        'date',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+     )
+ 
+admin.site.register(Order, OrderAdmin)
+# admin.site.register(OrderLineItem, OrderAdmin)
