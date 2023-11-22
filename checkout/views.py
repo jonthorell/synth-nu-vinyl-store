1be718f266e4093ffc3c70444abd1ac1c8be56d1
Jonas Thorell <jthorell@gmail.com>
2023-11-22 02:06:55 +0100

    Add: Further preparations for stripe, among which is importing the public key from the env-variables into the checkout view. Also some additional styling on the checkout form.

--- checkout/views.py
+++ checkout/views.py
@@ -11,17 +11,20 @@ import freecurrencyapi
 def checkout(request):
     client = freecurrencyapi.Client(settings.CURRENCY_API_KEY)
     result = client.currencies(currencies=['EUR', 'CAD', 'SEK'])
+    stripe_pk = settings.STRIPE_PUBLIC_KEY
     
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
-        return redirect(reverse('products'))
+        return redirect("/products/?genre=classic,electronica,ebm,experimental,industry,spop")
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'currency': result,
+        'stripe_public_key': stripe_pk,
+        'client_secret': 'test client secret',
     }
 
     
