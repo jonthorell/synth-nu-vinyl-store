a69124dc9bac54ff97c7fe9fd2e36c5105c54476
Jonas Thorell <jthorell@gmail.com>
2023-11-27 02:03:24 +0100

    Add: order history now shows a "Past order confirmation" header instead of the "Thank you" header that is shown when the order was initially processed. Additionally, the popup was removed since it was needlessly obtrusive and unneccsarry after this change.

--- profiles/views.py
+++ profiles/views.py
@@ -30,14 +30,10 @@ def profile(request):
     }
 
     return render(request, template, context)
-                  
+
+@check_user_is_member                  
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
-
-    messages.info(request, (
-        f'This is a past confirmation for order number {order_number}. '
-        'A confirmation email was sent on the order date.'
-    ))
 
     template = 'checkout/checkout_success.html'
     context = {
