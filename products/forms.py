e6857c31b7514c8e1d568f4b5e2dc621dcc11919
Jonas Thorell <jthorell@gmail.com>
2023-11-28 21:44:59 +0100

    Add: product form for superusers

---
+++ products/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import product, genre
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = genre.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['genre'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
