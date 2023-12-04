51a1d2e2e6bc959a02d6da3a72e377e6b9dd4829
Jonas Thorell <jthorell@gmail.com>
2023-12-04 10:51:06 +0100

    Update: some fixes in the webhook files, should fix (again) the e-mail confirmations.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -9,6 +9,7 @@ from profiles.models import UserProfile
 
 import json
 import time
+import stripe
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
@@ -18,15 +19,17 @@ class StripeWH_Handler:
         
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
-        cust_email = [order.email, ]
+        cust_email = order.email
         subject = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_subject.txt',
-            {'order': order})
+            "checkout/confirmation_emails/confirmation_email_subject.txt",
+            {"order": order},
+        )
         body = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_body.txt',
-            {'order': order, 'contact_email': settings.EMAIL_HOST_USER})
-        
-        send_mail( subject, body, settings.EMAIL_HOST_USER, cust_email )
+            "checkout/confirmation_emails/confirmation_email_body.txt",
+            {"order": order, "contact_email": settings.DEFAULT_FROM_EMAIL},
+        )
+
+        send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
 
     def handle_event(self, event):
         """
@@ -46,9 +49,12 @@ class StripeWH_Handler:
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
         
-        billing_details = intent.charges.data[0].billing_details
+        # Get the Charge object
+        stripe_charge = stripe.Charge.retrieve(intent.latest_charge)
+        
+        billing_details = stripe_charge.billing_details  # updated
         shipping_details = intent.shipping
-        grand_total = round(intent.charges.data[0].amount / 100, 2)
+        grand_total = round(stripe_charge.amount / 100, 2)  # updated
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
@@ -97,8 +103,12 @@ class StripeWH_Handler:
         if order_exists:
             self._send_confirmation_email(order)
             return HttpResponse(
-                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
-                status=200)
+                content=(
+                    f'Webhook received: {event["type"]} | SUCCESS: '
+                    "Verified order already in database"
+                ),
+                status=200,
+            )
         else:
             order = None
             try:
@@ -117,12 +127,22 @@ class StripeWH_Handler:
                 )
                 for item_id, item_data in json.loads(bag).items():
                     prod = product.objects.get(id=item_id)
-                    order_line_item = OrderLineItem(
-                        order=order,
-                        product=prod,
-                        quantity=item_data,
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=prod,
+                            quantity=item_data,
                         )
-                    order_line_item.save()
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data["items_by_size"].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=prod,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
             except Exception as e:
                 if order:
                     order.delete()
@@ -131,8 +151,12 @@ class StripeWH_Handler:
                     status=500)
         self._send_confirmation_email(order)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
-            status=200)
+            content=(
+                f'Webhook received: {event["type"]} | SUCCESS: '
+                "Created order in webhook"
+            ),
+            status=200,
+        )
 
     def handle_payment_intent_payment_failed(self, event):
         """
