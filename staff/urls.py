1a8f780594fb90462d9d4f1d4d52f3de03be191b
Jonas Thorell <jthorell@gmail.com>
2023-12-05 23:01:24 +0100

    Add: handle contact form messages is fully working

--- staff/urls.py
+++ staff/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('view_orders', views.view_orders, name='view_orders'),
     path("approve_review", views.approve_review, name="approve_review"),
     path('approve/<int:review_id>/', views.update_review, name='update_review'),
+    path("handle_messages", views.handle_contact_messages, name="handle_contact_messages"),
+    path('handled/<int:handled_id>/', views.handled, name='handled'),
     ]
--- staff/views.py
+++ staff/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 
 
 from products.models import testimonial
+from core.models import contact_message
 
 @check_user_is_staff
 def staff(request):
@@ -48,3 +49,25 @@ def update_review(request, review_id):
     testimonial.objects.filter(pk=review_id).update(approved=True)
     messages.success(request, 'Review approved!')
     return redirect('/products/?genre=classic,electronica,ebm,experimental,industry,spop,euro')
+
+@check_user_is_staff
+def handle_contact_messages(request):
+    '''Class used for handling contact messages '''
+    
+    # it is only handling handled/not handled. It could certainly be extended with stuff like "Waiting for user", "Out of scope"
+    # and other things for a bigger site
+
+    contact_messages = contact_message.objects.filter(complete=0).all()
+
+    context = {
+        'contact_messages': contact_messages,
+    }
+
+    return render(request, 'staff/handle_contact.html', context)
+
+@check_user_is_staff
+def handled(request, handled_id):
+    """ Update db with new approved status """
+    contact_message.objects.filter(pk=handled_id).update(complete=1)
+    messages.success(request, 'Message handled!')
+    return redirect('/')
